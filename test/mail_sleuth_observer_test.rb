27da09e74c7d44efda29386172c909a0442760fc
Nicolas Buduroi <nbuduroi@gmail.com>
2012-03-20 12:07:02 -0400

    Changed the test to check the observer.

--- test/mail_sleuth_observer_test.rb
+++ test/mail_sleuth_observer_test.rb
@@ -13,6 +13,21 @@ class MailSleuthObserverTest < ActionMailer::TestCase
 
     Notifier.welcome(joe.id).deliver!
     assert !ActionMailer::Base.deliveries.empty?
+
+    log = MessageLog.last
+
+    expected_attr = {
+      'content_type'       => 'text/html; charset=UTF-8',
+      'from'               => ['test@example.com'],
+      'to'                 => ['joe@example.com'],
+      'subject'            => 'Welcome Joe Foo',
+      'perform_deliveries' => true,
+      'charset'            => 'UTF-8'
+    }
+
+    assert_equal expected_attr, log.attributes.slice(*expected_attr.keys)
+    assert_equal Notifier, log.delivery_handler
+    assert log.errors.empty?
   end
 
 end
