e8b932294c94dd510996522f1ec2269af14f3a1e
Nicolas Buduroi <nbuduroi@gmail.com>
2012-03-20 12:06:05 -0400

    Fixed engine that was using the wrong hook.

--- lib/mail_sleuth/engine.rb
+++ lib/mail_sleuth/engine.rb
@@ -4,7 +4,7 @@ class MailSleuth::Engine < ::Rails::Engine
   isolate_namespace MailSleuth
 
   initializer 'mail_sleuth.register_observer' do
-    ActiveSupport.on_load :active_mailer do
+    ActiveSupport.on_load :action_mailer do
       ActionMailer::Base.register_observer(MailSleuth::Observer)
     end
   end
