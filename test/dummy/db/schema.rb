da80d001e03eb946bdfbb7c1d4a1b75ca02a4998
Nicolas Buduroi <nbuduroi@gmail.com>
2012-03-20 12:05:32 -0400

    Installed and ran the migration.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120319190548) do
+ActiveRecord::Schema.define(:version => 20120320154306) do
+
+  create_table "message_logs", :force => true do |t|
+    t.text     "to"
+    t.text     "errors"
+    t.boolean  "perform_deliveries"
+    t.boolean  "bounced"
+    t.boolean  "retryable"
+    t.string   "message_id"
+    t.string   "final_recipient"
+    t.string   "action"
+    t.string   "error_status"
+    t.string   "diagnostic_code"
+    t.string   "from"
+    t.string   "delivery_method"
+    t.string   "delivery_handler"
+    t.string   "subject"
+    t.string   "content_type"
+    t.string   "charset"
+    t.datetime "created_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string "name"
